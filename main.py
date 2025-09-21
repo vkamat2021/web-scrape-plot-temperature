5bf6e830b8609f463a4772c559396a4d0977bf55
Vin Kam <vinkamat10@gmail.com>
2025-09-22 01:18:57 +0530

    Create db table; store scraped and extracted data in db table; read data from table and plot on a web page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import selectorlib
 import requests
+import sqlite3
 
 # define URL of the webpage to be scraped
 URL = "https://programmer100.pythonanywhere.com/"
@@ -20,24 +21,41 @@ def extract(source):
     return value
 
 
-# function to store scraped temperatures into a file
+def init_db():
+    # Function to create database table if not existing already
+    with sqlite3.connect("data.db", timeout=10) as connection:
+        connection.execute("CREATE TABLE IF NOT EXISTS temperatures (datetime TEXT,temperature REAL)")
+
+
+# function to store scraped temperatures into SQL database table
 def store(extracted):
-    with open("data.txt", "a") as file:
-        current_datetime = datetime.now()
-        # timestamp = current_datetime.timestamp()    # timestamp[ step is not required
-        # local_time = datetime.fromtimestamp(timestamp)
-        formatted_time = current_datetime.strftime('%Y-%m-%d-%H-%M-%S')
-        # print(formatted_time)
-        file.write(f'{formatted_time}, {extracted} \n')
+    current_datetime = datetime.now()
+    formatted_time = current_datetime.strftime('%Y-%m-%d-%H-%M-%S')
+    row = [formatted_time, extracted]
+    with sqlite3.connect("data.db", timeout=10) as connection:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO temperatures VALUES(?,?)", row)
+        connection.commit()
 
 
+# code to store datae into a file instead
+# with open("data.txt", "a") as file:
+#     current_datetime = datetime.now()
+#     # timestamp = current_datetime.timestamp()    # timestamp[ step is not required
+#     # local_time = datetime.fromtimestamp(timestamp)
+#     formatted_time = current_datetime.strftime('%Y-%m-%d-%H-%M-%S')
+#     # print(formatted_time)
+#     file.write(f'{formatted_time}, {extracted} \n')
+
+# function to read data if reading from a file. This function should called from webapp.py
 # def read(extracted):
 #     with open("data.txt", "r") as file:
 #         return file.read()
 
-
+# code to execute the program steps
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    init_db()
     store(extracted)
--- webapp.py
+++ webapp.py
@@ -1,8 +1,23 @@
 import streamlit as st
-import pandas as pd
+import plotly.express as px
+import sqlite3
 
-# Plot temperature v/s date from the data.txt file using pandas and streamlit native line chart.
+# import pandas as pd. It is not used below
+
+# Plot temperature v/s date from the sqlite db table and with streamlit plotly line chart.
 # Plotly is better for charts
-df = pd.read_csv('data.txt')
-# print(df)
-st.line_chart(df, x='date', y='temperature')
+# Read date from DB.
+with sqlite3.connect("data.db", timeout=10) as connection:
+    cursor = connection.cursor()
+    cursor.execute("SELECT datetime, temperature FROM temperatures")
+    rows = cursor.fetchall()
+    # Data is returned as tuples hence converted to lists below
+    datetime = []
+    temperature = []
+    for row in rows:
+        datetime.append(row[0])
+        temperature.append(float(row[1]))
+# print(rows)
+# Plot data
+figure = px.line(x=datetime, y=temperature, labels={'x': 'DateTime', 'y': 'Temperature'})
+st.plotly_chart(figure)
